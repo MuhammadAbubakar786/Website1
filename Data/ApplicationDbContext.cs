5b22a3089a1db2e37c20e00796a1798262333672
MuhammadAbubakar786 <mianabubakar131998@gmail.com>
2020-12-24 08:15:09 +0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Website.Models;
+
+namespace Website.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Users> user { get; set; } 
+        
+    }
+}
